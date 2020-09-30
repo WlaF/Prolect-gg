6d4454a38cdf04900ef5e6a76ae88761c19ec09d
WlaF <starokozhko.slavik@gmail.com>
2020-09-30 11:30:43 +0300

    Update page9.php
    commit

--- page9.php
+++ page9.php
@@ -20,30 +20,6 @@ include 'header.php';
 
 <h1>Ця сторінка знаходиться на стадії розробки!</h1>
 
-<div class="content">
-	<input type="number" id="num1">
-	<br>
-	<input type="number" id="num2">
-	<p id="empty_for">
-
-   	</p>
-   	<br>
-   		<p>
-			   <button id="but_for">Вирахувати поправочний коефіцієнт на осьову складову сили різання, Kp</button>
-		   </p>
-   		<p>
-			   <button id="generate_for">Згенерувати формулу</button>
-		   </p>
-   		<p>
-			   <button  id="copyred_for" disabled="true">Скопіювати формулу</button><span id="show">Скопійовано!</span>
-		   </p>
-   	
-   	<p id="res_for">
-   		
-   	</p>
-
-</div>
-
 
 	   <?php 
 
